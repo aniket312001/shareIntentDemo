8d34875b7b02340d1b66bd7745e6ed7187106c2e
aniket <aniketchavan505@gmail.com>
2023-09-12 11:29:18 +0530

    Share Demo Done

--- lib/utils/api_urls.dart
+++ lib/utils/api_urls.dart
@@ -1,9 +1,7 @@
 class Urls {
   //Test environment base url
-  // static const String baseUrl = 'https://skinfotechies.in/demo/accounting/api';
-  static const String baseUrl = 'https://mijnkontinu.nl/api-new';
-  //Live environment base url
-  // static const String baseUrl = '';
+  static const String baseUrl = 'https://skinfotechies.in/demo/accounting/api';
+
   static const String loginServerCall = 'login.php';
   static const String notificationListServerCall =
       'fetch-notification-list.php';
